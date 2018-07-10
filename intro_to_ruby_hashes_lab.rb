fd3c26e6578f5f45d7690a85e043d3a9de4699d4
Peter Meister <prmeister89@gmail.com>
2018-07-10 18:03:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4,
-      :rent_in_dollars =>{}
-      :names => {}
+	    :pieces => {4},
+      :rent_in_dollars =>{},
+      :names => {},
 	  }
 	}
 	
